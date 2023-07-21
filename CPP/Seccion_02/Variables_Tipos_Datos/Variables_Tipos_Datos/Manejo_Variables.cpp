20789869f74715859abe1f10e97bf12135cf838b
KUSHIRO13 <kushiroamigama@gmail.com>
2023-07-21 16:19:05 -0500

    Agregar archivos de proyecto.

---
+++ CPP/Seccion_02/Variables_Tipos_Datos/Variables_Tipos_Datos/Manejo_Variables.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+
+using namespace std;
+
+int main() {
+	// Declarar variables
+	int entero = 10;
+	float flotante = 7.9;
+	// Imprimir variables
+	cout << "Entero: " << entero << "\nFlotante: " << flotante << endl;
+
+	return 0;
+}
